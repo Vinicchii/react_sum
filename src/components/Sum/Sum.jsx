ae8e166fe02c4367976b021d879be4df945d9929
Vinicius Maximiano <v.maximiano@hotmail.com>
2026-04-17 02:33:34 -0300

    fixed

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,7 +1,5 @@
-const Sum = ({ a = 0, b = 0 } = {}) => (
-  <p>
-    Sum of ${a} + ${b} is ${a + b}
-  </p>
+export const Sum = ({ a = 0, b = 0 } = {}) => (
+  <p>{`Sum of ${a} + ${b} is ${a + b}`}</p>
 );
 
 export default Sum;
